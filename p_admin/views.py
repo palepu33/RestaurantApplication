dc57c2e106bb38761291b7cbc391592df20a8a33
Ravindra <63154620+palepu33@users.noreply.github.com>
2020-04-16 20:36:52 +0530

    Initial commit

---
+++ p_admin/views.py
@@ -0,0 +1,162 @@
+from django.shortcuts import render,redirect
+from p_admin.models import *
+
+from django.contrib import messages
+from .forms import *
+def admin_login(request):
+    return render(request,"p_admin/login.html")
+
+
+def admin_login_check(request):
+    ausername = request.POST.get("admin_username")
+    apassword = request.POST.get("admin_password")
+    try:
+        AdminLoginModel.objects.get(username=ausername,password=apassword)
+        request.session['status'] = True
+        return redirect('admin_home')
+
+    except AdminLoginModel.DoesNotExist:
+        messages.error(request,"Sorry Invalid Details")
+        return redirect('admin_login')
+
+
+def admin_home(request):
+    return render(request, "p_admin/admin_home.html")
+
+
+def admin_logout(request):
+    request.session['status'] = False
+    return redirect('admin_login')
+
+
+def open_state(request):
+    return render(request,'p_admin/open_state.html',{"sf":StateForm(),"sdata":StateModel.objects.all()})
+
+
+def save_state(request):
+    sf = StateForm(request.POST)
+    if sf.is_valid():
+        sf.save()
+        return redirect('open_state')
+    else:
+        return render(request,"p_admin/open_state.html",{"sf":sf})
+
+
+def update_state(request):
+    sno = request.GET.get("sno")
+    sname = request.GET.get("sname")
+    d1 = {"sno":sno,"sname":sname}
+    return render(request,"p_admin/open_state.html",{"update_data":d1,"sdata":StateModel.objects.all()})
+
+
+def update_state_data(request):
+    sno = request.POST.get("s1")
+    sname = request.POST.get("s2")
+    StateModel.objects.filter(state_no = sno).update(state_name=sname)
+    return redirect('open_state')
+
+
+def delete_state(request):
+    sno = request.GET.get("sno")
+    StateModel.objects.filter(state_no=sno).delete()
+    return redirect('open_state')
+
+
+def open_city(request):
+    return render(request,'p_admin/open_city.html',{"sf":CityForm(),"cdata":CityModel.objects.all()})
+
+
+def save_city(request):
+    sf = CityForm(request.POST)
+    if sf.is_valid():
+        sf.save()
+        return redirect('open_city')
+    else:
+        return render(request, "p_admin/open_city.html", {"sf": sf})
+
+
+def update_city(request):
+    no = request.GET.get("sno")
+    cname = request.GET.get("sname")
+    d1 = {"cno": no, "cname": cname}
+    return render(request, "p_admin/open_city.html", {"update_data": d1,"cdata": CityModel.objects.all()})
+
+
+
+def update_city_data(request):
+    cno = request.POST.get("c1")
+    cname = request.POST.get("c2")
+    CityModel.objects.filter(city_no=cno).update(city_name=cname)
+    return redirect('open_city')
+
+
+
+def delete_city(request):
+    cno = request.GET.get("cno")
+    CityModel.objects.filter(city_no=cno).delete()
+    return redirect('open_city')
+
+
+
+def open_area(request):
+    return render(request, 'p_admin/open_area.html', {"sf": AreaForm(), "adata": AreaModel.objects.all()})
+
+
+def save_area(request):
+    sf = AreaForm(request.POST)
+    if sf.is_valid():
+        sf.save()
+        return redirect('open_area')
+    else:
+        return render(request, "p_admin/open_area.html", {"sf": sf})
+def update_area(request):
+    no = request.GET.get("ano")
+    aname = request.GET.get("aname")
+    d1 = {"ano": no, "aname": aname}
+    return render(request, "p_admin/open_area.html", {"update_data": d1, "adata": AreaModel.objects.all()})
+
+
+def update_area_data(request):
+    ano = request.POST.get("a1")
+    aname = request.POST.get("a2")
+    AreaModel.objects.filter(area_no=ano).update(area_name=aname)
+    return redirect('open_area')
+
+
+
+def delete_area(request):
+    ano = request.GET.get("ano")
+    AreaModel.objects.filter(area_no=ano).delete()
+    return redirect('open_area')
+
+def open_type(request):
+    return render(request, 'p_admin/open_type.html', {"sf": RestaurantTypeForm(), "tdata": RestaurantTypeModel.objects.all()})
+
+
+def save_type(request):
+    sf = RestaurantTypeForm(request.POST)
+    if sf.is_valid():
+        sf.save()
+        return redirect('open_type')
+    else:
+        return render(request, "p_admin/open_type.html", {"sf": sf})
+
+def update_type(request):
+    tno = request.GET.get("tno")
+    tname = request.GET.get("tname")
+    d1 = {"tno": tno, "tname": tname}
+    return render(request, "p_admin/open_type.html", {"update_data": d1, "tdata": RestaurantTypeModel.objects.all()})
+
+
+
+def update_type_data(request):
+    tno = request.POST.get("t1")
+    tname = request.POST.get("t2")
+    RestaurantTypeModel.objects.filter(no=tno).update(type_name=tname)
+    return redirect('open_type')
+
+
+def delete_type(request):
+    tno= request.GET.get("tno")
+    RestaurantTypeModel.objects.filter(no=tno).delete()
+    return redirect('open_type')
